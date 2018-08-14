561eaaf4d9f768c8c12d827e99b5c9366af91ae9
yang1015 <itsak1992@gmail.com>
2018-08-14 11:07:16 +0800

    update update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MiniGameWeb
+## 微信小程序web版本
--- js/base/ResourceLoader.js
+++ js/base/ResourceLoader.js
@@ -4,5 +4,6 @@ import { Resource } from "./Resource.js";
 // 资源文件加载器 确保 在所有图片都加载完成后 才渲染canvas
 export class ResourceLoader {
     constructor(){
+
     }
 }
